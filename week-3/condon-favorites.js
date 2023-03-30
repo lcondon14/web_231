fa6d02799dae82bb0904686e7247fb532b9c4c86
lcondon14 <122494018+lcondon14@users.noreply.github.com>
2023-03-30 14:43:42 -0500

    updated html, css and js files

--- week-3/condon-favorites.js
+++ week-3/condon-favorites.js
@@ -4,3 +4,20 @@
  * Date: 30 March 2023
  * Description: JavaScript page for assignment 3.2
  */
+
+
+function favTeam () {
+  alert('My favorite sports team is the Atlanta Braves!')
+}
+
+function favBook () {
+  alert('My favorite book is Gone Girl!')
+}
+
+function favAuthor () {
+  alert('My favorite author is Colleen Hoover1')
+}
+
+function favProLanguage () {
+  alert('My favorite programming language is not JavaScript!')
+}
