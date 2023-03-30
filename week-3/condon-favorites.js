790f2ae2c0cebe92822847ff86066f7ed6ce9578
lcondon14 <122494018+lcondon14@users.noreply.github.com>
2023-03-30 15:07:00 -0500

    added programmers comments

--- week-3/condon-favorites.js
+++ week-3/condon-favorites.js
@@ -5,19 +5,22 @@
  * Description: JavaScript page for assignment 3.2
  */
 
-
+/** The favTeam functions alerts popup box with favorite team message */
 function favTeam () {
   alert('My favorite sports team is the Atlanta Braves!')
 }
 
+/** The favBook function alerts a popup box with favorite book message */
 function favBook () {
   alert('My favorite book is Gone Girl!')
 }
 
+/** The favAuthor function alerts a popup box with favorite author message */
 function favAuthor () {
   alert('My favorite author is Colleen Hoover1')
 }
 
+/** The favProLanguage function alerts a pop up box with favorite author message */
 function favProLanguage () {
   alert('My favorite programming language is not JavaScript!')
 }
