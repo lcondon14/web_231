d29547a58257d7cd78872e3aa4b968cd5a205cac
lcondon14 <122494018+lcondon14@users.noreply.github.com>
2023-04-07 12:03:31 -0500

    added comments

--- week-4/condon-number-game.js
+++ week-4/condon-number-game.js
@@ -6,16 +6,18 @@
  */
 
 const rnd = generateRndNum()
-
+/** Generates random number between 1 and 10 */
 function generateRndNum() {
   return Math.floor((Math.random() * 10) + 1)
 }
-
+/** Messages to be displayed when user's guess is checked */
 function chkGuess () {
   const msgLessThan = "The number is less than " + document.getElementById('guessNum').value
   const msgGreaterThan = "The number is greater than " + document.getElementById('guessNum').value
   const msgCongrats = 'Congratulations! You picked the right number!'
   const guess = document.getElementById('guessNum').value
+
+/** Statements for the conditions against the check guess  */
 if (guess > rnd) {
   document.getElementById('test-results').innerHTML = msgLessThan
 } else if (guess < rnd) {
