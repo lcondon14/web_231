5d205fbcefced90a0bcd55d87074e0f79011e564
lcondon14 <122494018+lcondon14@users.noreply.github.com>
2023-05-02 01:38:09 -0500

    added comments

--- week-7/condon-calculator.js
+++ week-7/condon-calculator.js
@@ -4,56 +4,70 @@
  * Date: 26 April 2023
  * Description: JavaScript page for Calculator app
  */
-
+// Multiplying function
 function multiply (num1, num2) {
   if (typeof num1 !== 'number' || typeof num2 !== 'number') {
     throw new Error("Input must be numbers");
   }
+  // Checks if inputs are numbers
   if (isNaN(num1) || isNaN(num2)) {
+    // Error message thrown if not truthy
     throw new Error("Input must be numbers");
   }
+  // Results of input fields
   return num1 * num2;
 }
 
+// Division Function
 function divide(num1, num2) {
   if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+// Throws error message if not truthy
     throw new Error("Input must be numbers");
   }
+
+// Checks if inputs are numbers
   if (isNaN(num1) || isNaN(num2)) {
     throw new Error("Input must be numbers");
   }
+  // Error message thrown if user divides 0
   if (num1 === 0) {
     throw new Error("Cannot divide zero")
   }
+  // Error message if user divides by 0
   if (num2 === 0) {
     throw new Error("Cannot divide by zero");
   }
+
+// Results of input fields
   return num1 / num2;
 }
 
+// Button created for results
 const multiplyButton = document.getElementById("multiply-button");
 
 
 function displayMultiplyResults() {
+  // User input variables
   const input1 = document.getElementById('multi-one').value;
   const input2 = document.getElementById('multi-two').value;
   const num1 = parseFloat(input1);
   const num2 = parseFloat(input2);
-
+// Try statement for multiply function
   try {
   const result = multiply(num1, num2);
-  const resultElement = document.getElementById("mul-results");
+  // Displays user input results
+  const resultElement = document.getElementById("mul-results").innerHTML = num1 + ' * ' + num2 + ' = ' + result;
   resultElement.innerHTML = result;
-
+// Alert for error
   } catch (error) {
     alert(error.message);
   }
 }
 
-
+// Button created for division equation
 const divideButton = document.getElementById("divide-button");
 
-
+// Function for user input division equation
 function displayDivideResults() {
   const input1 = document.getElementById("div-one").value;
   const input2 = document.getElementById("div-two").value;
@@ -62,14 +76,16 @@ function displayDivideResults() {
 
   try {
   const result = divide(num1, num2);
+  // Displays results for division equation
   const resultElement = document.getElementById("div-results");
   resultElement.innerHTML = result;
-
+// Error message thrown
   } catch (error) {
     alert(error.message);
   }
 }
 
-
+// Onclick function created for multiply
 divideButton.onclick = displayDivideResults;
+// Onclick function created for divide
 multiplyButton.onclick = displayMultiplyResults;
